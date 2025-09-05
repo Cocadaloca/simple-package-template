3a869ce23808acec3e78eaff731c17ef424ee4d5
Fernando <juninhogozzer@gmail.com>
2025-09-04 21:10:05 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ with open("requirements.txt") as f:
 setup(
     name="package_name",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Cocadaloca",
+    author_email="fernandojunioror@gmail.com",
+    description="aula python",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/Cocadaloca/simple-package-template"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
